4a26df940b1ecd88f05f473b266c5a386714a41f
VaaVN <ne4valera@mail.ru>
2026-06-13 17:23:32 +0300

    footer

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,27 @@ document.addEventListener("DOMContentLoaded", () => {
         ><a href="entFair.html">Ярмарка деревьев</a
         ><a href="products.html  ">Товары</a>
       </div>`;
+  let footer = document.createElement("footer");
+  footer = document.body.appendChild(footer);
+  footer.innerHTML = `<div class="footerBody">
+        <div class="linksColumns">
+          <div class="linksColumn">
+            <a href="">Веб-плакат ЯД</a> <a href="">Веб-плакат ДКВ</a>
+            <a href="">Зин ДКВ</a>
+          </div>
+          <div class="linksColumn">
+            <a href="">Портфолио бренда</a>
+            <a href="">Портфолио сайта</a>
+            <a href="">Портфолио зина ДКВ</a>
+            <a href="">Портфолио плаката ДКВ</a>
+          </div>
+          <div class="linksColumn">
+            <p>Нечаев Валерий БРИЦП251</p>
+            <a href="">Моё портфолио</a> <a href="">Моё портфолио ШД</a>
+          </div>
+        </div>
+        <img src="assets/img/footerLogo.svg" />
+      </div>`;
 
   const links = document.querySelectorAll('[role="link"]');
   for (let link of links) {
